db04c4eddb39e55d4d8ec4deec78a232d56d772f
Vitor Nascimento <bielvitor2008@hotmail.com>
2021-02-17 21:32:32 -0300

    Projeto inicializado.

---
+++ app/src/main/java/me/vitornascimento/ceep/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.vitornascimento.ceep
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
